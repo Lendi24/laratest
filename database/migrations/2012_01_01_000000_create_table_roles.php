9cbd081f36a459659d87af21af6ae60b5942e8b6
Lendi24 <Lendic24@hotmail.com>
2023-01-22 17:31:58 +0100

    oi

--- database/migrations/2012_01_01_000000_create_table_roles.php
+++ database/migrations/2012_01_01_000000_create_table_roles.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('desc');
+            $table->string('description');
+            $table->string('color');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,18 +22,23 @@ class DatabaseSeeder extends Seeder {
 class RoleSeeder extends Seeder{
     public function run(){
         DB::table('roles')->insert([ //1
-            'name' => "admin",
-            'desc' => "",    
+            'name'          => "admin",
+            'description'   => "",    
+            'color'         => "bg-red-600",    
         ]);
 
         DB::table('roles')->insert([ //2
-            'name' => "poweruser",
-            'desc' => "",    
+            'name'          => "poweruser",
+            'description'   => "",    
+            'color'         => "bg-blue-600",    
         ]);
 
         DB::table('roles')->insert([ //3
-            'name' => "user",
-            'desc' => "",    
+            'name'          => "user",
+            'description'   => "",    
+            'color'         => "'flex",    //testing html-class injection
+
+            //'color'         => "bg-green-400",    Auth::user();
         ]);
     }
 }
@@ -51,6 +56,32 @@ class UserSeeder extends Seeder{
             'remember_token' => "",
 
             'role_id' => 1,    
+        ]);
+
+        DB::table('users')->insert([ //2
+            'name' => "powerusr",
+            'phone_number' => '0000000001',
+    
+            'first_name' => fake()->name(),
+            'last_name'  => fake()->name(),
+    
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => "",
+
+            'role_id' => 2,    
+        ]);
+
+        DB::table('users')->insert([ //3
+            'name' => "user",
+            'phone_number' => '0000000002',
+    
+            'first_name' => fake()->name(),
+            'last_name'  => fake()->name(),
+    
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => "",
+
+            'role_id' => 3,    
         ]);
     }
 }
